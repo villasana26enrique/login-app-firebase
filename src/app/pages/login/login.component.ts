452eccc188bdf66968dfdce47322a0ee7db5bcaa
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-29 21:23:21 -0400

    Obteniendo la información de los campos, de la pantalla Login y su validación

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  usuario: UsuarioModel;
+
   constructor() { }
 
   ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
+
+  login(form: NgForm) {
+    if ( form.invalid ) { return; }
+    console.log('El Form se ha enviado con Exito');
+    console.log(this.usuario);
   }
 
 }
